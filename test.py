12e292016d960cbd962fa64b851a3bb51ea85639
deansk90 <deansk90@gmail.com>
2018-08-15 23:06:56 +0300

    Updating test.py with feature scaling and accuracy printing
    In this commit the following were added:
    1. The method for performing feature scaling and actual calls to this function
    2. Printing not only the prediction itself, but the score according to the accuracy metric

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import os
 import  pandas as pd
 from experiments_manager import ExperimentsManager
+from sklearn.preprocessing import MinMaxScaler
 from device_session_classifier import DeviceSessionClassifier
 from device_sequence_classifier import DeviceSequenceClassifier
+from sklearn import metrics
 
 device = 'watch'
-other_device = 'motion_sensor'
+other_device = 'thermostat'
 model_pkl = r'models/{0}/{0}_cart_entropy_50_samples_leaf.pkl'.format(device)
 dataset_csv = 'data/validation.csv'
 sc = DeviceSequenceClassifier(device, model_pkl, is_model_pkl=True)
+
+def perform_feature_scaling(x_train):
+    scaler = MinMaxScaler()
+    scaler.fit(x_train)
+    return scaler.transform(x_train)
 
 
 m = sc.load_model_from_pkl(model_pkl)
@@ -17,8 +24,10 @@ validation = sc.load_data_from_csv(dataset_csv)
 
 
 all_sess = validation.drop(sc.y_col, axis=1).values
+all_sess = perform_feature_scaling(all_sess)
 other_dev_sess = validation.groupby(sc.y_col).get_group(other_device)
 other_dev_sess = other_dev_sess.drop(sc.y_col, axis=1).values
+other_dev_sess = perform_feature_scaling(other_dev_sess)
 
 sc.find_opt_seq_len(validation)
 seq_len = 7
@@ -27,7 +36,11 @@ seqs = [other_dev_sess[i:i+seq_len] for i in range(len(other_dev_sess)-seq_len)]
 # seq_len = 1
 # seqs = [sessions[0:seq_len]]
 
+classification = 1 if device == other_device else 0
+
+y_actual = [classification] * 100
 x = m.predict(other_dev_sess)
+print(metrics.accuracy_score(y_actual,x))
 pred = sc.predict(seqs)
 print(pred)
 print("YAYY!!")
